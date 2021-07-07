32703ab78342dbe95acfbb7f291f485d91e4b65c
Gudrun Søborg Vinther <gudrun.vinther@gmail.com>
2021-07-07 21:49:03 +0200

    x > y

---
+++ HKKursus/src/modul1GrundlæggendeJava/StoerreEnd.java
@@ -0,0 +1,139 @@
+package modul1GrundlæggendeJava;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ * Opgave 1.3.2 Lav en brugergrænseflade, hvor man kan indtaste to tal. Der skal
+ * også være en knap. Når man trykker på knappen, skal programmet udskrive true
+ * hvis det første tal er større end det andet, og false hvis det ikke er.
+ * 
+ * Opgave 1.3.3 Ret løsningen fra opgave to, så programet udskriver Sandt i
+ * stedet for true og Falsk i stedet for false.
+ * 
+ * Tip: Brug ?:-operatoren
+ */
+public class StoerreEnd extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StoerreEnd frame = new StoerreEnd();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StoerreEnd() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		JLabel lblNewLabel = new JLabel("Indtast tal x");
+		JLabel lblNewLabel_1 = new JLabel("Indtast tal y");
+		JLabel lblNewLabel_3 = new JLabel("x > y: ");
+		JLabel lblNewLabel_2 = new JLabel("");
+
+		textField = new JTextField();
+		textField_1 = new JTextField();
+
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+				int x = Integer.parseInt(textField.getText());
+				int y = Integer.parseInt(textField_1.getText());
+
+				String s = (x > y) ? "Sandt" : "Falsk";
+
+				lblNewLabel_2.setText(s);
+				} 
+				catch (NumberFormatException nfe) {
+					System.out.println("Input var ikke en int");
+				}
+			}
+		});
+
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 1;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		contentPane.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 2;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 3;
+		contentPane.add(lblNewLabel_3, gbc_lblNewLabel_3);
+
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 3;
+		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
+	}
+
+}
