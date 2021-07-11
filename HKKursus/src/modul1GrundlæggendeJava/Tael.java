2152f3b5f335b5f4a505fe8262857c511db273e4
Gudrun Søborg Vinther <gudrun.vinther@gmail.com>
2021-07-11 19:33:20 +0200

    Optælling

---
+++ HKKursus/src/modul1GrundlæggendeJava/Tael.java
@@ -0,0 +1,99 @@
+package modul1GrundlæggendeJava;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Tael extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tael frame = new Tael();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Tael() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblTekst = new JLabel("Indtast tal, som der skal tælles op til");
+		JLabel lblResultat = new JLabel("");
+		textField = new JTextField();
+		JButton btnNewButton = new JButton("Optæl");
+		
+		GridBagConstraints gbc_lblTekst = new GridBagConstraints();
+		gbc_lblTekst.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTekst.anchor = GridBagConstraints.EAST;
+		gbc_lblTekst.gridx = 0;
+		gbc_lblTekst.gridy = 0;
+		contentPane.add(lblTekst, gbc_lblTekst);
+
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String s = "";
+				
+				for (int i = 1; i < Integer.parseInt(textField.getText()); i++) {		
+					
+					s += i + " ";
+				}
+				
+				lblResultat.setText(s);
+				
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 1;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+		
+		GridBagConstraints gbc_lblResultat = new GridBagConstraints();
+		gbc_lblResultat.gridx = 1;
+		gbc_lblResultat.gridy = 2;
+		contentPane.add(lblResultat, gbc_lblResultat);
+	}
+
+}
