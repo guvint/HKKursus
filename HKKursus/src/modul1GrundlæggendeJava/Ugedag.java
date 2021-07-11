5869bb4d9d85177707ff116db2d538aa640fa3dd
Gudrun Søborg Vinther <gudrun.vinther@gmail.com>
2021-07-11 19:23:13 +0200

    Hvilken ugedag er det i dag? (switch case)

---
+++ HKKursus/src/modul1GrundlæggendeJava/Ugedag.java
@@ -0,0 +1,98 @@
+package modul1GrundlæggendeJava;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.util.Calendar;
+
+/**
+ * Opgave 1.4.6 Lav et program, som skriver hvilken ugedag det er. Brug en
+ * switch case til det.
+ * 
+ * (Hvis du googler godt nok, finder du en meget nem måde at skrive ugedagen,
+ * men i denne opgave skal du gøre det med en switch case)
+ */
+public class Ugedag extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ugedag frame = new Ugedag();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ugedag() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 0, 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		JLabel lblNewLabel = new JLabel("Hvilken ugedag er det?");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.gridx = 2;
+		gbc_lblNewLabel_1.gridy = 0;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+		Calendar calendar = Calendar.getInstance();
+		int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+		switch (day) {
+		case 2:
+			lblNewLabel_1.setText("Mandag");
+			break;
+		case 3:
+			lblNewLabel_1.setText("Tirsdag");
+			break;
+		case 4:
+			lblNewLabel_1.setText("Onsdag");
+			break;
+		case 5:
+			lblNewLabel_1.setText("Torsdag");
+			break;
+		case 6:
+			lblNewLabel_1.setText("Fredag");
+			break;
+		case 7:
+			lblNewLabel_1.setText("Lørdag");
+			break;
+		case 1:
+			lblNewLabel_1.setText("Søndag");
+			break;
+		}
+	}
+
+}
